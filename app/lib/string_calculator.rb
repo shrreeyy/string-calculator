c5bb3bd9a8f2b226231d71b9fe3ff15c7fc0e1a9
Shrey <shrey018@gmail.com>
2025-04-28 23:09:02 +0530

    feat: implement add method to handle comma-delimited numbers

--- app/lib/string_calculator.rb
+++ app/lib/string_calculator.rb
@@ -2,6 +2,12 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
+    # If there is no comma, return the number itself
     numbers.to_i unless numbers.include?(",")
+
+    # Handle multiple numbers by splitting the string by commas
+    nums = numbers.split(",").map(&:to_i)
+
+    nums.sum
   end
 end
