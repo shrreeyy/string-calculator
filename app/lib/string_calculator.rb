d710d092526af698b7ded532dc59b640b6c1e3e7
Shrey <shrey018@gmail.com>
2025-04-28 23:26:11 +0530

    feat: add support for custom delimiters

--- app/lib/string_calculator.rb
+++ app/lib/string_calculator.rb
@@ -2,17 +2,30 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    # If there is no comma, return the number itself
-    numbers.to_i unless numbers.include?(",")
-
-    # Handle multiple numbers by splitting the string by commas
     delimiter = ","
 
-    # Replace newlines with commas
-    numbers   = numbers.gsub("\n", delimiter)
+    # Handle custom delimiter if present
+    if numbers.start_with?("//")
+      delimiter, numbers = parse_custom_delimiter(numbers)
+    end
 
-    nums      = numbers.split(delimiter).map(&:to_i)
+    # Replace newlines with the chosen delimiter
+    numbers = numbers.gsub("\n", delimiter)
+
+    # If there is no comma or newline, return the number itself
+    return numbers.to_i unless numbers.include?(delimiter)
+
+    # Handle multiple numbers by splitting the string by the delimiter
+    nums = numbers.split(delimiter).map(&:to_i)
 
     nums.sum
+  end
+
+  private
+
+  def self.parse_custom_delimiter(numbers)
+    parts = numbers.split("\n", 2)
+    delimiter = parts[0][2..-1]
+    [ delimiter, parts[1] ]
   end
 end
