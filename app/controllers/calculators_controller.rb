19531ffc85bc82b9f1a4846f8a4bb81d746eee08
Shrey <shrey018@gmail.com>
2025-04-29 22:58:57 +0530

    feat: add view with input form and result display

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -4,9 +4,19 @@ class CalculatorsController < ApplicationController
   def calculate
     input_string = params[:input_string]
 
-    input_string = input_string.strip.gsub(/^"|"$/, "")
+    begin
+      input_string = input_string.strip.gsub(/^"|"$/, "")
 
-    @result = StringCalculator.add(input_string)
+      input_string = input_string.gsub("\\n", "\n")
+
+      if input_string.match?(/[a-zA-Z]/) && !input_string.start_with?("//")
+        raise "invalid input: unsupported characters"
+      end
+
+      @result = StringCalculator.add(input_string)
+    rescue RuntimeError => e
+      @error = e.message
+    end
 
     respond_to do |format|
       format.html { render :index }
